4e8e4ec7256b221a7f278c572de9d975bb4c60ac
Avinash <59113057+Viwarrior@users.noreply.github.com>
2021-03-02 22:12:08 +0530

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,7 +6,7 @@ var value = status["on"];
 else{
 localStorage.setItem('nailItStatus','{"on":1}');
 var value = 1;
-} window.onunload = function(){localStorage.setItem('nailItStatus','{"on":0}');}
+} window.onunload = function(){localStorage.setItem('nailItStatus','{"on":1}');}
 if(value){
 var h = setInterval(function(){var x = document.getElementsByClassName("style-scope yt-img-shadow"); for(var i =1; i<x.length; i++){x[i].style.filter = " brightness(0) saturate(80%)";}},2000);
 localStorage.setItem('nailItStatus','{"on":0}');
